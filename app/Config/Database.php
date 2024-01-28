48edd72dc433a69f437b41357e04b3aa41e8e30a
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2024-01-28 21:02:04 +0700

    remove comment config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -16,7 +16,6 @@ class Database
     public static function getConnection(string $env = 'test'): PDO
     {
         if (self::$pdo == null) {
-            // create new PDO
             require_once __DIR__ . '/../../config/database.php';
             $config = getDatabaseConfig();
             self::$pdo = new PDO(
